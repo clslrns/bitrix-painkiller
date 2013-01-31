addf4064cd1022c006581325432e9d5455b04c22
Vitaly Chirkov <chirkov@thelikers.ru>
2013-01-31 21:48:08 +0400

    find_name optimization

--- bitrix-painkiller.py
+++ bitrix-painkiller.py
@@ -7,24 +7,10 @@ class BitrixPainkillerCommand( sublime_plugin.TextCommand ):
     def find_name( self, view ):
         """Find name of bitrix component on the left from cursor."""
         end = view.sel()[0].end()
-        if end < 3:
-            return ''
-        begin = end - 3
-
-        lastMatch = ''
-        while begin:
-            regionContent = view.substr( sublime.Region( begin, end ) )
-            # Component name match
-            matchRes = re.match( '(\s|^)(([a-zA-Z_-]+):([a-zA-Z._-]+))', regionContent )
-            if matchRes:
-                lastMatch = matchRes.group(2)
-            elif lastMatch or re.search( '\s', regionContent ):
-                # Finish the iteration when we found one name 
-                # or there is a space in search region
-                begin = 1
-            begin -= 1
-
-        return lastMatch
+        
+        regionContent = view.substr( sublime.Region( 0, end ) )
+        matchRes = re.match( '(^|\s)(([a-zA-Z_-]+):([a-zA-Z._-]+))', regionContent )
+        return matchRes.group(2)
 
     def get_component_signature( self, componentName ):
         pass
