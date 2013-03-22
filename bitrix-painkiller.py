a918ee024230aada97328dc94b00f046c6818504
Vitaly Chirkov <chirkov@thelikers.ru>
2013-03-22 18:09:23 +0400

    Host detection based on file

--- bitrix-painkiller.py
+++ bitrix-painkiller.py
@@ -3,6 +3,7 @@ import sublime, sublime_plugin
 import re
 import httplib, urllib
 import json
+import os
 
 __version__      = '0.1'
 __core_version__ = '0.1'
@@ -25,9 +26,46 @@ class BitrixPainkillerCommand( sublime_plugin.TextCommand ):
         )
         return matchRes.group('cname') if matchRes else None
 
-    def get_component_signature( self, componentName ):
-        conn = httplib.HTTPConnection('zitar.dev')
-        conn.request( 'GET', '/bp.php?component=' + componentName )
+    def file_get_contents( self, filename ):
+        """ http://stackoverflow.com/questions/1432126/how-to-get-content-of-a-small-ascii-file-in-python"""
+        with open(filename) as f:
+            return f.read()
+
+    def get_host( self, filepath ):
+        """ Find hostname file and get host for any file in webroot directory."""
+        pathChunks = filepath.split( '/bitrix/' )
+
+        if len( pathChunks ) > 1:
+            # Найдена директория bitrix
+            # Проверим, есть ли в ней наш модуль
+            pathChunks.pop()
+            currentPath = ''
+            for chunk in pathChunks:
+                currentPath += chunk + '/bitrix/'
+                filename = currentPath + 'modules/thelikers.painkiller/site_host'
+                if( os.path.exists( filename ) ):
+                    host = self.file_get_contents( filename )
+                    if( host ):
+                        return host
+
+        pathChunks = filepath.split('/')
+        pathChunks.pop()
+
+        currentPath = ''
+        for chunk in pathChunks:
+            currentPath += chunk + '/'
+
+            filename = currentPath + 'bitrix/modules/thelikers.painkiller/site_host'
+            if( os.path.exists( filename ) ):
+                host = self.file_get_contents( filename )
+                if( host ):
+                    return host
+
+        return false
+
+    def get_component_signature( self, componentName, host ):
+        conn = httplib.HTTPConnection( host )
+        conn.request( 'GET', '/bitrix/tools/painkiller_component_signature.php?component=' + componentName )
         response = conn.getresponse()
         componentParams = json.loads( response.read() )
         conn.close()
@@ -36,6 +74,10 @@ class BitrixPainkillerCommand( sublime_plugin.TextCommand ):
     def run( self, edit, block = False ):
         view = self.view
         path = view.file_name()
+        host = self.get_host( path )
+
+        if not host:
+            return false
 
         for cursorId, cursorPos in enumerate( view.sel() ):
             name = self.find_name(view, cursorId)
@@ -43,13 +85,11 @@ class BitrixPainkillerCommand( sublime_plugin.TextCommand ):
             if not name:
                 continue
 
-            requestResult = self.get_component_signature( name )
+            requestResult = self.get_component_signature( name, host )
 
             view.erase( edit, view.find( name, view.sel()[cursorId].begin() - len(name) ) )
             pref = ' ' * ( view.sel()[cursorId].begin() \
                            - view.line( view.sel()[cursorId].begin() ).begin() )
-
-            print dir( requestResult )
 
             params = u''
             if requestResult['status'] == 'found':
